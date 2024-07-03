a83ce7655c6ffda50b1f21c45ce1f916e946ed6a
vabdalla <victorabdallaa@gmail.com>
2024-07-02 23:31:40 -0300

    teste

---
+++ Conteudo/Fatiamento de String.py
@@ -0,0 +1,30 @@
+#fatiando da posição 9 a posição 21
+frase = 'Curso em Video Python'
+##print(frase[9:22])
+
+#fatiando da posição 9 a posição 21 pulando de 2 ao 2
+#print(frase[9:22:2])
+
+#fatiando da posição 0 ate 5
+#print(frase[:5])
+
+#fatiando da posição 15 ate o final
+#print(frase[15:])
+
+#fatiando da posição 9 ate final de 3 em 3
+#print(frase[9::3])
+
+#saber tamanho da frase função len
+#print(len(frase))
+
+#para contar quantas vezes aparece a letra o
+#print(frase.count('o'))
+
+#para contar quantas vezes aparece a letra 'o' da posição 0 ate 12
+#print(frase.count('o',0,13))
+
+#para saber se encontrou a string 'deo' e em qual posição começa
+#print(frase.find('deo'))
+
+#existe 'Curso' em frase? - nao é uma funcionalidade, é um operador
+print('Curso' in frase)
