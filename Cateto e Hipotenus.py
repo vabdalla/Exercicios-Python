aa28404e762201b1d730ff14064a0d103fc587d7
vabdalla <victorabdallaa@gmail.com>
2024-07-02 22:48:47 -0300

    teste

--- Cateto e Hipotenus.py
+++
@@ -1,5 +0,0 @@
-import math
-catop = float(input('Digite o comprimento do cateto oposto: '))
-catad = float(input('Cateto Adjascente: '))
-hip = math.sqrt((math.pow(catop,2)) + (math.pow(catad,2)))
-print(f'A hipotenusa irá medir: {hip}')
--- Operadores aritimetics.py
+++
@@ -1,4 +0,0 @@
-num = int(input('Digite um número: '))
-print(f'O dobro de {num} vale {num*2}')
-print(f'O triplo de {num} vale {num*3}')
-print(f'A raiz quadrada de {num} vale {num**(1/2)}')
