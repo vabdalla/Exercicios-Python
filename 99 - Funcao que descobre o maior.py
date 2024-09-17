8a14dbde749cd91eb52ed36a5c92ce67e5a0b33e
Victor Abdalla <victorabdallaa@gmail.com>
2024-09-16 23:05:44 -0300

    teste novo pc

--- 99 - Funcao que descobre o maior.py
+++ 99 - Funcao que descobre o maior.py
@@ -1,18 +1,18 @@
 import random
 
 def maior(* num):
-    maior = 0
+    maior_num = 0
     #   Gera 6 linhas
     for c in range (0,6):
     #   Gera de 0 a 6 numeros aleatorios
         for d in range(0,random.randint(0,7)):
-            aleatorio = random.randint(1, 10)
+            aleatorio = random.randint(0, 10)
     #       Encontra o maior
-            if aleatorio > maior:
-                maior=aleatorio
-                aleatorio=maior
+            if aleatorio > maior_num:
+                maior_num=aleatorio
+
             print(aleatorio, end= ' ')
         print(f'Foram informados {d+1} valores ao todo')
-        print(f'O maior valor foi {maior}')
-        maior = 0
-maior(10)
+        print(f'O maior valor foi {maior_num}')
+        maior_num = 0
+maior()
